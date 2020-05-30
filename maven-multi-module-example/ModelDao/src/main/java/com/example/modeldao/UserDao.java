edb6eb5e33fb75afae13c92651bfb0feebcb6b91
Romarjo Zeka <romarjozeka@gmail.com>
2020-05-30 22:23:35 +0300

    Implement UserDaoApi interface on UserDao in ModelDao

--- maven-multi-module-example/ModelDao/src/main/java/com/example/modeldao/UserDao.java
+++ maven-multi-module-example/ModelDao/src/main/java/com/example/modeldao/UserDao.java
@@ -1,5 +1,33 @@
 package com.example.modeldao;
+import com.example.daoapi.UserDaoApi;
 
-public class UserDao{
+import java.util.List;
+import java.util.Optional;
 
+public class UserDao implements UserDaoApi <User>{
+
+    @Override
+    public List<User> getAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<User> get(long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void save(User user) {
+
+    }
+
+    @Override
+    public void update(User user, String[] params) {
+
+    }
+
+    @Override
+    public void delete(User user) {
+
+    }
 }
